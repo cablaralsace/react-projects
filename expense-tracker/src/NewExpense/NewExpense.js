fefbb4a0f16ce466c42d427900d0500a8cf80573
Alsace Cablar <cablaralsace@gmail.com>
2021-06-22 03:59:39 +0800

    Add Expense Form

--- expense-tracker/src/NewExpense/NewExpense.js
+++ expense-tracker/src/NewExpense/NewExpense.js
@@ -1,8 +1,21 @@
 import "./NewExpense.css"
 import ExpenseForm from "./ExpenseForm"
+import { useState } from "react"
 
 const NewExpense = (props) => {
 
+    //new state for button, mainly for UI
+    const [isFormActive, setFormActive] = useState (false); // DEFAULT useState (false) meaning hindi kita yung form
+
+    const formActiveHandler = () => { //STATE: makikita yung form
+        setFormActive (true);
+    }
+    
+    const formInactiveHandler = () => { //STATE: hidden yung form
+        setFormActive (false);
+    }
+
+    // for saving data
     const saveExpenseDataHandler = (enteredExpenseData) => {
         // console.log(enteredExpenseData);
         const expenseData = {
@@ -11,10 +24,17 @@ const NewExpense = (props) => {
         }
 
         props.onAddExpense(expenseData);
+        setFormActive(false); //after mag-submit ng bagong expenseData, hindi ulit makikita  yung form
     }
     return (
         <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isFormActive && //SCENARIO 1: kung hindi active yung form, button lang ang makikita
+            <button onClick={formActiveHandler}> Add New Expense </button>}
+
+            {isFormActive && //SCENARIO 2: kung active, makikita yung mismong form na may submit and cancel button, pero pagpindot ng cancel, magiging inactive ulit yung form
+            <ExpenseForm
+            onSaveExpenseData={saveExpenseDataHandler}
+            onCancel={formInactiveHandler}/>}
         </div>
     )
 }
